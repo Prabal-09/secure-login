a0858acc05a4e1640259d35305ad782acc2eb474
@ANJALI PRAKASH <anjaliprakash78177@gmail.com>
2025-06-27 12:52:49 +0530

    Update captcha.php

--- captcha.php
+++ captcha.php
@@ -1,14 +1,24 @@
 <?php
 session_start();
 
-$code = rand(1000, 9999);
-$_SESSION["captcha"] = $code;
 
-$image = imagecreate(80, 30);
+$characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+$captcha_length = 6;
+$captcha_code = '';
+
+for ($i = 0; $i < $captcha_length; $i++) {
+    $captcha_code .= $characters[rand(0, strlen($characters) - 1)];
+}
+
+
+$_SESSION["captcha"] = $captcha_code;
+
+$image = imagecreate(150, 40);
 $bg = imagecolorallocate($image, 255, 255, 255);
 $text_color = imagecolorallocate($image, 0, 0, 0);
 
-imagestring($image, 5, 15, 8, $code, $text_color);
+imagestring($image, 5, 35, 10, $captcha_code, $text_color);
+
 
 header("Content-type: image/png");
 imagepng($image);
