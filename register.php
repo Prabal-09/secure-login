eddf5d43369485c3ab04b9f9c93ac81be4bdcfa6
Bhaskar054 <155009693+Bhaskar82268@users.noreply.github.com>
2025-06-27 09:04:14 +0530

    register.php

--- register.php
+++ register.php
@@ -1,10 +1,17 @@
 <?php
 $conn = new mysqli("localhost", "root", "", "login_demo");
+
 if (isset($_POST['submit'])) {
     $username = $_POST['username'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    $conn->query("INSERT INTO users (username, password) VALUES ('$username', '$password')");
-    echo "Registered securely!";
+   $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+    if ($conn->query($sql)) {
+        
+        header("Location: login.html"); 
+        exit();
+    } else {
+        echo "Error: " . $conn->error;
+    }
 }
 ?>
